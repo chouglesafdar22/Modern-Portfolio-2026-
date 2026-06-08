68f2e2f5d0140b2e81dfbd5e3b2b781f5a468f70
Safdar Chougle <chouglesafdar22@gmail.com>
2026-06-08 23:18:10 +0530

    changed

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -127,7 +127,7 @@ export default function ProjectsSection() {
                                                     text={"Live Demo"}
                                                     icon={<FaArrowUpRightFromSquare />}
                                                     className="md:w-auto w-full"
-                                                    contentClassName="xl:text-base md:text-sm text-[11px] md:w-auto w-full"
+                                                    contentClassName="xl:text-base text-sm md:w-auto w-full"
                                                 />
                                                 <Button
                                                     href={project.github}
